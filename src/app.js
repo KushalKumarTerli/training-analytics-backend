692054546178f0e1f12f546897071db680da7061
KushalKumarTerli <kushalkumarterli@gmail.com>
2026-01-16 23:16:41 +0530

    feat: bootstrap express app and server entry point

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express') 
+
+const  app = express();
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send({message: "Training Analytics Backend is running" });
+})
+
+module.exports = app;
